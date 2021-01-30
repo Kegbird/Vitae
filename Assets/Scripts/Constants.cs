5a4ba95aa04b5094c0449315eb46286e757f9f16
Kegbird <lazyduckdev@gmail.com>
2021-01-30 08:08:35 +0100

    Main menu implemented.

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public static class Constants
+    {
+        public static int SPRING_SCENE;
+        public static int SUMMER_SCENE;
+        public static int FALL_SCENE;
+        public static int WINTER_SCENE;
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using Assets.Scripts;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    public Image black_screen;
+    [SerializeField]
+    public bool interaction_enabled;
+
+    private void Start()
+    {
+        FadeBlackScreen();
+    }
+
+    public void FadeBlackScreen()
+    {
+        IEnumerator FadeBlackScreen()
+        {
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                black_screen.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+            interaction_enabled = true;
+        }
+
+        StartCoroutine(FadeBlackScreen());
+    }
+
+    public void AppearBlackScreen()
+    {
+        interaction_enabled = false;
+        IEnumerator AppearBlackScreen()
+        {
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                black_screen.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+        }
+        StartCoroutine(AppearBlackScreen());
+    }
+
+    public void PlayBtnClicked()
+    {
+        if (!interaction_enabled)
+            return;
+
+        IEnumerator Delay()
+        {
+            AppearBlackScreen();
+            yield return new WaitForSeconds(1f);
+            SceneManager.LoadScene(Constants.SPRING_SCENE);
+        }
+        StartCoroutine(Delay());
+    }
+
+    public void QuitBtnClicked()
+    {
+        if (!interaction_enabled)
+            return;
+
+        IEnumerator Delay()
+        {
+            AppearBlackScreen();
+            yield return new WaitForSeconds(1f);
+            Application.Quit();
+        }
+        StartCoroutine(Delay());
+    }
+}
